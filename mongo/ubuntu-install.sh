065e5fcf76d7725362898c3e2f880ca485e2e87b
Herman <actiniwang@gmail.com>
2020-11-18 15:08:01 +0800

    Update and rename install-mongo.sh to mongo/ubuntu-install.sh

--- mongo/ubuntu-install.sh
+++ mongo/ubuntu-install.sh
@@ -5,7 +5,7 @@ hold=${1:-nohold}
 sudo apt-get install gnupg
 wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
 
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu $(lsb_release -cs)/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
 
 sudo apt-get update
 sudo apt-get install -y mongodb-org=4.4.1 mongodb-org-server=4.4.1 mongodb-org-shell=4.4.1 mongodb-org-mongos=4.4.1 mongodb-org-tools=4.4.1
