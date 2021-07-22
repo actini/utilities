ac4030e3a31eb81a2d02c6ca7f24f3d2ac305bec
Herman <actini@outlook.com>
2021-07-22 19:29:16 +0800

    Update ubuntu-install.sh

--- mongo/ubuntu-install.sh
+++ mongo/ubuntu-install.sh
@@ -3,7 +3,7 @@
 
 version=${1:-"latest"}
 
-sudo apt-get install gnupg
+sudo apt-get install -y gnupg
 
 wget -qO - https://www.mongodb.org/static/pgp/server-4.0.asc | sudo apt-key add -
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
@@ -15,7 +15,7 @@ echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu $(lsb_release
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu $(lsb_release -cs)/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu $(lsb_release -cs)/mongodb-org/5.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list
 
-sudo apt-get update
+sudo apt-get -y update
 
 if [ $version == "latest" ]
 then
