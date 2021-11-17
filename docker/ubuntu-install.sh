f4cd9d878a09650a9d7525f645568f5875565bc1
Herman <actini@outlook.com>
2021-11-17 21:52:45 +0800

    Update ubuntu-install.sh
    Remove docker version

--- docker/ubuntu-install.sh
+++ docker/ubuntu-install.sh
@@ -9,7 +9,7 @@ add-apt-repository   "deb [arch=amd64] https://download.docker.com/linux/ubuntu 
   $(lsb_release -cs) \
   stable"
 
-apt-get update && apt-get install -y   containerd.io=1.2.10-3   docker-ce=5:19.03.4~3-0~ubuntu-$(lsb_release -cs)   docker-ce-cli=5:19.03.4~3-0~ubuntu-$(lsb_release -cs)
+apt-get update && apt-get install -y containerd.io docker-ce docker-ce-cli
 
 cat > /etc/docker/daemon.json <<EOF
 {
